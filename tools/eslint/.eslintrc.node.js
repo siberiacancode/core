b69d8a2787c8fbbfc9c1af185716a07f923100c4
debabin <dmitriybabinup@gmail.com>
2024-02-16 18:15:26 +0700

    🧊 update packages

--- tools/eslint/.eslintrc.node.js
+++ tools/eslint/.eslintrc.node.js
@@ -35,7 +35,7 @@ module.exports = {
           // External packages:
           ['^react', '^@?\\w'],
           // Internal packages:
-          ['^@(siberiacancodepay-core/.*|$)'],
+          ['^@(siberiacancode-core/.*|$)'],
           // Alias imports:
           ['^@(([\\/.]?\\w)|assets|test-utils)'],
           // Side effect imports:
--- tools/eslint/.eslintrc.react.js
+++ tools/eslint/.eslintrc.react.js
@@ -56,7 +56,7 @@ module.exports = {
           // External packages:
           ['^react', '^@?\\w'],
           // Internal packages:
-          ['^@(siberiacancodepay-core/.*|$)'],
+          ['^@(siberiacancode-core/.*|$)'],
           // Alias imports:
           ['^@(([\\/.]?\\w)|assets|test-utils)'],
           // Side effect imports:
