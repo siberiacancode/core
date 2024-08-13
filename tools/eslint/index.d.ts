a41c0546a8ea06602cef4af98050532b4d10009e
debabin <dmitriybabinup@gmail.com>
2024-08-13 18:28:31 +0700

    release/eslint/2.0.0 🧊 add eslint 9

---
+++ tools/eslint/index.d.ts
@@ -0,0 +1,18 @@
+declare module '@siberiacancode/eslint' {
+  declare type Eslint = (
+    options?: import('@antfu/eslint-config').OptionsConfig & {
+      'jsx-a11y': boolean;
+    } & import('@antfu/eslint-config').TypedFlatConfigItem,
+    ...userConfigs: import('@antfu/eslint-config').Awaitable<
+      | import('@antfu/eslint-config').TypedFlatConfigItem
+      | import('@antfu/eslint-config').TypedFlatConfigItem[]
+      | import('@antfu/eslint-config').FlatConfigComposer<any, any>
+      | Linter.Config[]
+    >[]
+  ) => import('@antfu/eslint-config').FlatConfigComposer<
+    import('@antfu/eslint-config').TypedFlatConfigItem,
+    import('@antfu/eslint-config').ConfigNames
+  >;
+
+  export const eslint: Eslint;
+}
--- tools/eslint/index.js
+++ tools/eslint/index.js
@@ -1,4 +1,98 @@
-const eslintReact = require('./.eslintrc.react.js');
-const eslintNode = require('./.eslintrc.node.js');
+import antfu from '@antfu/eslint-config';
+import pluginReact from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
 
-module.exports = { eslint: { react: eslintReact, node: eslintNode } };
+/** @type {import('@siberiacancode/eslint').Eslint} */
+export const eslint = (...args) =>
+  antfu(
+    {
+      react: true,
+      typescript: true,
+      jsx: true
+    },
+    {
+      name: 'siberiacancode/rewrite',
+      rules: {
+        'style/semi': 'off',
+        'style/jsx-one-expression-per-line': 'off',
+        'style/member-delimiter-style': 'off',
+        'style/jsx-quotes': ['error', 'prefer-single'],
+        'style/comma-dangle': 'off',
+        'style/arrow-parens': 'off',
+        'style/quote-props': 'off',
+
+        'antfu/top-level-function': 'off',
+        'antfu/if-newline': 'off',
+        'antfu/curly': 'off',
+
+        'react-hooks/exhaustive-deps': 'off',
+
+        'test/prefer-lowercase-title': 'off',
+
+        'no-console': 'warn'
+      }
+    },
+    {
+      name: 'siberiacancode/react',
+      plugins: {
+        'plugin-react': pluginReact
+      },
+      settings: {
+        react: {
+          version: 'detect'
+        }
+      },
+      rules: {
+        'plugin-react/function-component-definition': [
+          'error',
+          {
+            namedComponents: ['arrow-function'],
+            unnamedComponents: 'arrow-function'
+          }
+        ]
+      }
+    },
+    {
+      name: 'siberiacancode/imports',
+      plugins: {
+        'simple-import-sort': simpleImportSort
+      },
+      rules: {
+        'sort-imports': 'off',
+        'import/order': 'off',
+        'import/extensions': 'off',
+        'simple-import-sort/exports': 'error',
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              ['^react', '^@?\\w'],
+              ['^@(siberiacancode-core/.*|$)'],
+              ['^@(([\\/.]?\\w)|assets|test-utils)'],
+              ['^\\u0000'],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              ['^.+\\.s?css$']
+            ]
+          }
+        ]
+      }
+    },
+    ...args.reduce((acc, config) => {
+      if (config['jsx-a11y']) {
+        acc.push({
+          ...pluginJsxA11y.flatConfigs.recommended,
+          name: 'siberiacancode/jsx-a11y'
+        });
+        return acc;
+      }
+
+      acc.push({
+        ...config,
+        name: `siberiacancode/${config.name}`
+      });
+
+      return acc;
+    }, [])
+  );
