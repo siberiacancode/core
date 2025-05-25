659455d1be7b6ff3f428e20af88723bf828a78eb
debabin <dmitriybabinup@gmail.com>
2025-05-25 15:03:17 +0700

    main 🧊 update version, fetches migrate to vite

--- packages/fetches/vite.config.js
+++ packages/fetches/vite.config.js
@@ -1,5 +1,47 @@
-/** @type {import('vitest').InlineConfig} */
-module.exports = {
-  globals: true,
-  environment: 'jsdom'
-};
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+import pkg from './package.json';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist/types',
+      tsconfigPath: './tsconfig.json'
+    })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: pkg.name,
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'src/index.ts')
+      },
+      output: [
+        {
+          format: 'es',
+          dir: 'dist/esm',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          entryFileNames: '[name].mjs'
+        },
+        {
+          format: 'cjs',
+          dir: 'dist/cjs',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          entryFileNames: '[name].cjs',
+          exports: 'named'
+        }
+      ]
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  }
+});
