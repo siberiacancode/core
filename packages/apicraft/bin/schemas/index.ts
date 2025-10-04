5b15de20badafb90d8d0ec3c93a57c153c7c0342
vitrivdolkom <vdy0555@gmail.com>
2025-10-04 23:06:18 +0700

    fix bugs

--- packages/apicraft/bin/schemas/index.ts
+++ packages/apicraft/bin/schemas/index.ts
@@ -50,47 +50,49 @@ export const apicraftOptionSchema = z
           .optional()
       })
     ),
-    filters: z.object({
-      deprecated: z.boolean().default(true).optional(),
-      operations: z
-        .object({
-          exclude: z.array(z.string()).readonly().optional(),
-          include: z.array(z.string()).readonly().optional()
-        })
-        .optional(),
-      orphans: z.boolean().default(false).optional(),
-      parameters: z
-        .object({
-          exclude: z.array(z.string()).readonly().optional(),
-          include: z.array(z.string()).readonly().optional()
-        })
-        .optional(),
-      preserveOrder: z.boolean().default(false).optional(),
-      requestBodies: z
-        .object({
-          exclude: z.array(z.string()).readonly().optional(),
-          include: z.array(z.string()).readonly().optional()
-        })
-        .optional(),
-      responses: z
-        .object({
-          exclude: z.array(z.string()).readonly().optional(),
-          include: z.array(z.string()).readonly().optional()
-        })
-        .optional(),
-      schemas: z
-        .object({
-          exclude: z.array(z.string()).readonly().optional(),
-          include: z.array(z.string()).readonly().optional()
-        })
-        .optional(),
-      tags: z
-        .object({
-          exclude: z.array(z.string()).readonly().optional(),
-          include: z.array(z.string()).readonly().optional()
-        })
-        .optional()
-    }),
+    filters: z
+      .object({
+        deprecated: z.boolean().default(true).optional(),
+        operations: z
+          .object({
+            exclude: z.array(z.string()).readonly().optional(),
+            include: z.array(z.string()).readonly().optional()
+          })
+          .optional(),
+        orphans: z.boolean().default(false).optional(),
+        parameters: z
+          .object({
+            exclude: z.array(z.string()).readonly().optional(),
+            include: z.array(z.string()).readonly().optional()
+          })
+          .optional(),
+        preserveOrder: z.boolean().default(false).optional(),
+        requestBodies: z
+          .object({
+            exclude: z.array(z.string()).readonly().optional(),
+            include: z.array(z.string()).readonly().optional()
+          })
+          .optional(),
+        responses: z
+          .object({
+            exclude: z.array(z.string()).readonly().optional(),
+            include: z.array(z.string()).readonly().optional()
+          })
+          .optional(),
+        schemas: z
+          .object({
+            exclude: z.array(z.string()).readonly().optional(),
+            include: z.array(z.string()).readonly().optional()
+          })
+          .optional(),
+        tags: z
+          .object({
+            exclude: z.array(z.string()).readonly().optional(),
+            include: z.array(z.string()).readonly().optional()
+          })
+          .optional()
+      })
+      .optional(),
     instance: z.union([instanceNameSchema, instanceSchema]).optional()
   })
   .strict();
